145609aa99401e0e347d64466914798243376130
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2024-11-08 21:53:02 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
 const { Pool } = require('pg');
+require('dotenv').config(); // This loads your .env file with DATABASE_URL for production
 
-// Configure the PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'web_card_db',
-  password: 'Theo2905#',
-  port: 5432, // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL, // Use the DATABASE_URL provided by Vercel
+  ssl: {
+    rejectUnauthorized: false, // Vercel (and other cloud services) require SSL
+  },
 });
 
+// Test the connection
 pool.connect((err) => {
-    if (err) {
-      console.error('Error connecting to PostgreSQL database:', err);
-    } else {
-      console.log('Connected to PostgreSQL database');
-    }
+  if (err) {
+    console.error('Error connecting to PostgreSQL database:', err);
+  } else {
+    console.log('Connected to PostgreSQL database');
+  }
 });
-
 
 module.exports = pool;
