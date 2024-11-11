7d3d5434245c1fc7b79d049a5b438f67e0564ff0
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2024-11-11 09:21:47 +0700

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config(); // This loads your .env file with DATABASE_URL for p
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // Use the DATABASE_URL provided by Vercel
   ssl: {
-    rejectUnauthorized: false, // Vercel (and other cloud services) require SSL
+    rejectUnauthorized: true,
   },
 });
 
